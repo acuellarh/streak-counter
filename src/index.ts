a8e3645175d5c994466a85e9be39f83e3970dec7
Alejandro <acuellarh@gmail.com>
2023-02-04 17:15:51 -0500

    Add write getStreak function

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,19 @@ interface Streak {
 
 const KEY = 'streak'
 
-export function streakCounter(storage: Storage, date: Date): Streak 
-{
+export function streakCounter(storage: Storage, date: Date): Streak {
+
+  const streakInLocalStorage = storage.getItem(KEY);
+
+  if (streakInLocalStorage) {
+    try {
+    const streak = JSON.parse(streakInLocalStorage || "");
+    return streak;
+    } catch (error) {
+      console.error("Failed to parse streak from localStorage");
+    }
+  }
+
   const streak ={
     currentCount: 1,
     startDate: formattedDate(date),
